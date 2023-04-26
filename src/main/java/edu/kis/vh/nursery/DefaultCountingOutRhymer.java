fc08812fdeb44605c1fbedb516c50649e48bac69
hubertgajdowski <240662@edu.p.lodz.pl>
2023-04-26 22:04:14 +0200

    changes after review

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,11 +2,12 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    public static final int INT = -1;
-    public static final int INT1 = 12;
-    private int[] numbers = new int[INT1];
+    public static final int NUMBERS_EMPTY = -1;
+    public static final int NUMBERS_QUANTITY = 12;
+    public static final int NUMBERS_FULL = NUMBERS_QUANTITY - 1;
+    private int[] numbers = new int[NUMBERS_QUANTITY];
 
-    public int total = INT;
+    public int total = NUMBERS_EMPTY;
 
     public void countIn(int in) {
         if (!isFull())
@@ -14,24 +15,24 @@ public class DefaultCountingOutRhymer {
     }
 
     public boolean callCheck() {
-        return total == INT;
+        return total == NUMBERS_EMPTY;
     }
 
 
     public boolean isFull() {
-        return total == 11;
+        return total == NUMBERS_FULL;
     }
 
 
     protected int peekaboo() {
         if (callCheck())
-            return INT;
+            return NUMBERS_EMPTY;
         return numbers[total];
     }
 
     public int countOut() {
         if (callCheck())
-            return INT;
+            return NUMBERS_EMPTY;
         return numbers[total--];
     }
 
--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -1,9 +1,11 @@
 package edu.kis.vh.nursery.list;
 
 public class IntLinkedList {
+    private static final Node LIST_EMPTY = null;
+    private static final boolean LIST_NOT_FULL = false;
+    private static final int LIST_NULL = -1;
+    private Node last;
 
-    Node last;
-    int i;
 
     public void push(int i) {
         if (last == null)
@@ -16,22 +18,22 @@ public class IntLinkedList {
     }
 
     public boolean isEmpty() {
-        return last == null;
+        return last == LIST_EMPTY;
     }
 
     public boolean isFull() {
-        return false;
+        return LIST_NOT_FULL;
     }
 
     public int top() {
         if (isEmpty())
-            return -1;
+            return LIST_NULL;
         return last.value;
     }
 
     public int pop() {
         if (isEmpty())
-            return -1;
+            return LIST_NULL;
         int ret = last.value;
         last = last.prev;
         return ret;
